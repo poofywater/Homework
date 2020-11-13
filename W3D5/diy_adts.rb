b31af1c2f9fa2ec76fffc9f4c66e21de74e63605
Derek Chen <derekchen72@gmail.com>
2020-11-13 10:05:10 -0500

    updated diy_adts

--- W3D5/diy_adts.rb
+++ W3D5/diy_adts.rb
@@ -1,17 +1,43 @@
 class Stack
     def initialize
       # create ivar to store stack here!
+      @stack = []
     end
 
     def push(el)
       # adds an element to the stack
+      @stack << el
     end
 
     def pop
       # removes one element from the stack
+      @stack.pop
     end
 
     def peek
       # returns, but doesn't remove, the top element in the stack
+      return @stack[-1]
     end
+  end
+
+  class Queue 
+    def initialize
+        @array = []
+    end
+
+    def enqueue(ele)
+        @array.unshift(ele) #add stuff
+    end
+
+    def dequeue(ele)
+        @array.pop #remove last
+    end
+
+    def peek(ele)
+        return @array[-1] #check out the last thing in the queue
+    end
+  end
+
+  class Map
+
   end
